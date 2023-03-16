b84da32057971989892464e036e76bf7509ce853
Andrew Dickinson <andrewjd@amazon.com>
2023-03-16 02:04:17 -0400

    Switch to file_upload_v2

--- src/mesh_supportbot_list/post_to_slack.py
+++ src/mesh_supportbot_list/post_to_slack.py
@@ -11,11 +11,11 @@ from slack_sdk import WebClient
 def post_to_slack(report_header: str, diagnostics_str: str):
     slack_client = WebClient(os.environ["SLACK_BOT_TOKEN"])
 
-    resp = slack_client.files_upload(
+    resp = slack_client.files_upload_v2(
         title=report_header,
         filename=report_header + ".md",
         content=diagnostics_str,
-        channels=[os.environ["SLACK_SUPPORT_CHANNEL_ID"]],
+        channels=os.environ["SLACK_SUPPORT_CHANNEL_ID"],
         initial_comment=" ".join(
             ["<@%s>" % uid for uid in os.environ["SLACK_USERS_TO_AT"].split(",")]
         ),
