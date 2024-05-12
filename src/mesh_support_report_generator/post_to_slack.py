ec3c45a525a0d631375b1f1e90f2be407b76e335
Andrew Dickinson <andrewjd@amazon.com>
2024-05-12 16:23:32 -0400

    Add ability for no users to be @ed

--- src/mesh_support_report_generator/post_to_slack.py
+++ src/mesh_support_report_generator/post_to_slack.py
@@ -18,6 +18,8 @@ def post_to_slack(report_header: str, diagnostics_str: str):
         channel=os.environ["SLACK_CHANNEL_ID"],
         initial_comment=" ".join(
             ["<@%s>" % uid for uid in os.environ["SLACK_USERS_TO_AT"].split(",")]
+            if os.environ["SLACK_USERS_TO_AT"]
+            else []
         ),
     )
     return f"https://app.slack.com/client/{resp['file']['user_team']}/{os.environ['SLACK_CHANNEL_ID']}"
